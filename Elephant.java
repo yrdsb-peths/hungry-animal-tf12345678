4cc0a52661dc333417f4ecbe6b391e1649ba6c2b
tf12345678 <150176267+tf12345678@users.noreply.github.com>
2023-11-30 12:49:06 -0500

    Added Elephant idle animations

--- Elephant.java
+++ Elephant.java
@@ -9,6 +9,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("231873-d2d5b364-22b4-49f5-bf2b-76ed5b338a6e.mp3");
+    GreenfootImage idle [] = new GreenfootImage[8];
+    
+    public Elephant()
+    {
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
     
     public void act()
     {
@@ -23,6 +40,7 @@ public class Elephant extends Actor
         
         // Eviscerate apple if the Elephant touches it
         eat();
+        animateElephant();
     }
     
     /**
