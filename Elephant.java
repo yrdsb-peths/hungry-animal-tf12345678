04a85476fbab5b0a5dbce9ba59b410722ebb0ca9
tf12345678 <150176267+tf12345678@users.noreply.github.com>
2023-12-01 12:59:40 -0500

    Made the elephant trunk animation slower

--- Elephant.java
+++ Elephant.java
@@ -13,7 +13,7 @@ public class Elephant extends Actor
     GreenfootImage idleLeft [] = new GreenfootImage[8];
     
     boolean facingRight = true;
-    
+    SimpleTimer animationTimer = new SimpleTimer();
     public Elephant()
     {
         for(int i = 0; i < idle.length; i++)
@@ -29,12 +29,21 @@ public class Elephant extends Actor
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(100, 100);
         }
+        
+        animationTimer.mark();
+        
         setImage(idle[0]);
     }
     
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
         if(facingRight == true)
         {
             setImage(idle[imageIndex]);
